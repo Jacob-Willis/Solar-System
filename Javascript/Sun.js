a78459637ddfd20293eff5cc9cf84f8640588a68
Jacob_Willis <jacebly@gmail.com>
2020-06-30 21:52:06 +1000

    Name changes

--- Javascript/Sun.js
+++ Javascript/Sun.js
@@ -1,6 +1,6 @@
 function Sun(radius, fragments) {
   var geometry = new THREE.SphereGeometry(radius, fragments, fragments);
-  var material = new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('/Resources/Textures/Sun_Texture.jpg') });
+  var material = new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('Resources/Textures/Sun_Texture.jpg') });
   material.color = new THREE.Color(1, 1, 1);
 
   var sphere = new THREE.Mesh(geometry, material);
