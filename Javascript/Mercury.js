e8ab03f7fb9d95633f521dd864e494f7a1d7ea9b
Jacob_Willis <jacebly@gmail.com>
2020-06-30 21:56:08 +1000

    File name changes

--- Javascript/Mercury.js
+++ Javascript/Mercury.js
@@ -1,6 +1,6 @@
 function Mercury(radius, fragments) {
   var geometry = new THREE.SphereGeometry(radius, fragments, fragments);
-  var material = new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('Resources/Textures/Mercury_Texture.jpg') });
+  var material = new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('../Resources/Textures/Mercury_Texture.jpg') });
   material.color = new THREE.Color(1, 1, 1);
 
   var sphere = new THREE.Mesh(geometry, material);
