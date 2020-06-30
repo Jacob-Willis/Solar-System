96e1e982c5b2ee40fb80ff310862abc7ede50817
Jacob_Willis <jacebly@gmail.com>
2020-06-30 21:59:59 +1000

    file name change

--- Javascript/Venus.js
+++ Javascript/Venus.js
@@ -1,9 +1,9 @@
 function Venus(radius, fragments) {
   var geometry = new THREE.SphereGeometry(radius, fragments, fragments);
-  var material = new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('../Resources/Textures/Venus_Texture.jpg') });
+  var material = new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('./Resources/Textures/Venus_Texture.jpg') });
   material.color = new THREE.Color(1, 1, 1);
 
-  console.log("HELLO");
+  console.log("hey there");
 
   var sphere = new THREE.Mesh(geometry, material);
   sphere.name = "Venus";
